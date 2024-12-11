992950a718ef694b46e8d64a3c486826b366bf67
Gleb <fedorov22134@gmail.com>
2024-12-11 20:47:47 +0300

    RobMartinHello!!!!

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ class AlgoDedMorozHouseClasterGifts:
         self.__num_of_cluster = 1
         self.result: list = []
 
+    def __get_first_gift(self) -> list:
+        return self.__gifts_information.pop(0)
+
     def cluster_gifts(self, max_weight=100) -> None:
         self.__sorted_by_ded_moroz_house()
         self.__algorithm_optimal_delivery()
@@ -46,18 +49,15 @@ class AlgoDedMorozHouseClasterGifts:
         self.__append_weight()
         self.__current_gift_information = self.__get_first_gift()
 
+    def __append_weight(self) -> None:
+        self.__current_weight += self.__current_gift_information[-1]
+
     def __is_correct_weight(self) -> bool:
         return self.__current_gift_information[-1] + self.__current_weight < 100
     
     def __add_to_general_cluster(self) -> None:
         self.__clusters.append(self.__current_cluster)
         self.__clear_data()
-
-    def __append_weight(self) -> None:
-        self.__current_weight += self.__current_gift_information[-1]
-
-    def __get_first_gift(self) -> list:
-        return self.__gifts_information.pop(0)
 
     def __clear_data(self) -> None:
         self.__current_weight = 0
